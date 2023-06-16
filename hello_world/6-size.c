89dfe601ce0a8578df3e4885d670bcaeb6619301
Replit user <>
2023-06-16 22:33:20 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void) {
     printf("size of a char: %zu bytes\n", sizeof(char));
-    printf("size of a int: %zu bytes\n", sizeof(int));
+    printf("size of an int: %zu bytes\n", sizeof(int));
 	printf("size of a long int: %zu bytes\n", sizeof(long int));
     printf("size of a long long int: %zu bytes\n", sizeof(long long int));
 	printf("size of a float : %zu bytes\n", sizeof(float));
