1f7f9153fd3c3a31275618093b9c6e4cb77159ca
Replit user <>
2023-06-16 22:24:37 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("The size of int is: %zu bytes\n", sizeof(int));
-    printf("The size of float is: %zu bytes\n", sizeof(float));
-    printf("The size of double is: %zu bytes\n", sizeof(double));
     printf("The size of char is: %zu bytes\n", sizeof(char));
+    printf("The size of int is: %zu bytes\n", sizeof(int));
+	printf("The size of long int is: %zu bytes\n", sizeof(long int));
+    printf("The size of long long int is: %zu bytes\n", sizeof(long long int));
+	printf("The size of float is: %zu bytes\n", sizeof(float));
+    printf("The size of double is: %zu bytes\n", sizeof(double));
 
-    return 0;
+    return (0);
 }
