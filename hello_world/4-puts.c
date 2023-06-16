50cbe3c4be605d54a667f893255e826bdbaaf92e
xiomara13 <jamara0917@gmail.com>
2023-06-16 14:19:43 -0700

    put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-char mensaje[] = "Programming is like building a multilingual puzzle\n"; 
+char mensaje[] = "\"Programming is like building a multilingual puzzle\n"; 
 puts(mensaje);
 
 return 0; 
