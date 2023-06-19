fd7cebb539d090f9198063ae46e17557816633d6
Replit user <>
2023-06-19 23:09:22 +0000

    cero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void) {
+	srand(time(0));  // Inicializar la semilla para generar números aleatorios
 
-int main() {
-    srand(time(0));  // Inicializar la semilla para generar números aleatorios
+	int n = rand() % 201 - 100;  // Generar un número aleatorio entre -100 y 100
 
-    int n = rand() % 201 - 100;  // Generar un número aleatorio entre -100 y 100
+	printf("El número %d es ", n);
 
-    printf("El número %d es ", n);
+	if (n > 0) {
+		printf("positivo\n");
+	} else if (n < 0) {
+		printf("negativo\n");
+	} else {
+		printf("cero\n");
+	}
 
-    if (n > 0) {
-        printf("positivo\n");
-    } else if (n < 0) {
-        printf("negativo\n");
-    } else {
-        printf("cero\n");
-    }
-
-    return 0;
+	return (0);
 }
