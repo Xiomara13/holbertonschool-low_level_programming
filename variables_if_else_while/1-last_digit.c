1d2a74d87cd3119e27f27f6aeb4c95245cfc4136
Replit user <>
2023-06-20 03:01:56 +0000

    uno

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	srand(time(0));  // Inicializar la semilla para generar números aleatorios
+
+	int n = rand();  // Generar un número aleatorio
+
+	printf("Último dígito de %d es ", n);
+
+	int ultimoDigito = n % 10;
+
+	printf("%d ", ultimoDigito);
+
+	printf("y es ");
+
+	if (ultimoDigito > 5)
+	{
+		printf("mayor que 5\n");
+    }
+	else if (ultimoDigito == 0)
+	{
+		printf("0\n");
+    }
+	else
+	{
+		printf("menor que 6 y no 0\n");
+    }
+
+    return 0;
+}
 
