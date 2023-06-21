54b07d0900304653031bd765a441ab5fd18e427a
xiomara13 <jamara0917@gmail.com>
2023-06-21 10:15:34 -0700

    dos

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,14 @@
 void print_alphabet_x10(void)
 {
 	/*Code to print the alphabet 10 times*/
-	char letter[] = "abcdefghijklmnñopqrstuvwxyz\n";
+	char letter;
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j = 0;
-
-		while (letter[j] != '\0')
-		{
-			_putchar(letter[j]);
-			j++;
-		}
+		for (letter = 'a'; letter <= 'z';)
+		_putchar(letter);
+			letter++;
 	}
 	_putchar('\n');
 }
