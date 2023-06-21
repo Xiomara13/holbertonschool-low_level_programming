8abea6ba774b33a119b441ed7ec87da8f710273b
xiomara13 <jamara0917@gmail.com>
2023-06-21 06:33:53 -0700

    uno

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,27 +1,22 @@
-#include <stdio>
+#include <stdio.h>
 #include <unistd.h>
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet - writes the character c to stdout
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  *
- * Prints the alphabet in lowercase, followed by a new line.}
+ *Prints the alphabet in lowercase, followed by a new line
  */
-void print_alphabet(void);
 void print_alphabet(void)
 {
 	/*Code to print the alphabet*/
 	char letter;
 
-	for (letter == 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
 	_putchar('\n');
-}
-{
-	print_alphabet(void);
-	return (write(1, &c, 1));
 }
