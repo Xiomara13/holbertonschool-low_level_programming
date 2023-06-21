e721b1907318ec261f2be9019d5515a3605b8b05
xiomara13 <jamara0917@gmail.com>
2023-06-21 09:18:17 -0700

    dos

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,17 @@ void print_alphabet_x10(void)
 	/*Code to print the alphabet 10 times*/
 	char letter;
 	int i;
+	int j;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 10; i++)
+		for (j = 0; j <26; j++)
 		{
 			_putchar(letter);
-		}
+			letter++;
+			}
+		letter = 'a';
+		/*Reset the letter to 'a' for the next line*/
 	}
 	_putchar('\n');
 }
