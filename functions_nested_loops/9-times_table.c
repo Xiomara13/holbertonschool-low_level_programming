8fa3e7a269399fb69423153e57c28424fdcffda8
xiomara13 <jamara0917@gmail.com>
2023-06-23 13:21:48 -0700

    nueve

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,13 +6,20 @@
  */
 void times_table(void)
 {
+	/*function code*/
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-		printf("%2d, ", i * j);
+			printf("%2d", i * j);
+			{
+				if (j != 9)
+				{
+					printf(", ");
+				}
+			}
 		}
 		printf("\n");
 	}
