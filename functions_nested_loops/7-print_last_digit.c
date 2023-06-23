329b7e26abf3e81736ec37df9b79ee5181e6c84c
xiomara13 <jamara0917@gmail.com>
2023-06-23 09:06:22 -0700

    siete

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,10 @@ int print_last_digit(int u)
 {
 	int last_digit = u % 10;
 
+	if (last_digit < 0)
+	{
+		last_digit *= -1; /*Si es negativo, convertirlo a positivo*/
+	}
 	_putchar(last_digit + '0');
-			return (last_digit);
+	return (last_digit);
 }
