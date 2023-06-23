e8b8aacbecaa995651c24c3c8174f8be9605423f
xiomara13 <jamara0917@gmail.com>
2023-06-23 11:01:45 -0700

    diez

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - check that adds two integers and returns the result
+ * @a: check the integer
+ * @b: check the integer
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
