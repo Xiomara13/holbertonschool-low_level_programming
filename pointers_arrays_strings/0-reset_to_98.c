448607e269864181855e40ac4a9234e6eb8cb1bc
xiomara13 <jamara0917@gmail.com>
2023-06-26 12:59:20 -0700

    beterry

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: check parameter and updates the value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
