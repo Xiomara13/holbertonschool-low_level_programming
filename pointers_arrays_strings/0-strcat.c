9f2692b4897cea31b6ac767a7083c330d2be0745
xiomara13 <jamara0917@gmail.com>
2023-06-27 07:56:45 -0700

    str

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: overwrit the terminating null byte
+ * @src: string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
