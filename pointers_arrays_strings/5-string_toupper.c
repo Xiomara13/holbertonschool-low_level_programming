548c7bc75e9dd119908bd0f37abaae3e9bb4bce2
xiomara13 <jamara0917@gmail.com>
2023-06-27 15:29:43 -0700

    string

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters
+ * @str: argument and returns a pointer
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
+	return (str);
+}
