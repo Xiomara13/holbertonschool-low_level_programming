20261e3abb4c1d7d2024395071517a63964f27fc
xiomara13 <jamara0917@gmail.com>
2023-06-29 15:42:24 -0700

    put

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,20 +8,13 @@
 void puts_half(char *str)
 {
 	int length = strlen(str);
-	int start_index;
+	int start_index = length / 2;
 	int i;
 
-	if (length % 2 == 0)
-	{
-		start_index = length / 2;
-	}
-	else
-	{
-		start_index = (length - 1) / 2;
-	}
 	for (i = start_index; i < length; i++)
 	{
 		putchar(str[i]);
 	}
+
 	putchar('\n');
 }
