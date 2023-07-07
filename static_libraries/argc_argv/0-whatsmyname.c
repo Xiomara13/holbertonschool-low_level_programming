6702345ab4d18d97c57838230fa00df893b27ced
xiomara13 <jamara0917@gmail.com>
2023-07-06 19:26:04 -0700

    argc

---
+++ static_libraries/argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints it name
+ * @argc: number of arguments
+ * @argv: represents the first element
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
