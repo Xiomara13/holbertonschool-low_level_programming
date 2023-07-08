40036b5d2ece0422907e0fbfceeda1a677ef3553
xiomara13 <jamara0917@gmail.com>
2023-07-08 12:56:29 -0700

    number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function
+ * @argc: number of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
