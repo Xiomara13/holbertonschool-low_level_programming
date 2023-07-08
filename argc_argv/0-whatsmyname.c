6a5ddd688d2563ccd3980e838fabf8041037f2c3
xiomara13 <jamara0917@gmail.com>
2023-07-08 12:26:19 -0700

    name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: contens the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
