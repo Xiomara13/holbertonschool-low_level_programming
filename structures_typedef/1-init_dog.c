757bf7842ff6ec920cdd1d252d39da86d748672a
xiomara13 <jamara0917@gmail.com>
2023-07-17 09:55:00 -0700

    dogg

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - function
+ * @d: pointer
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
