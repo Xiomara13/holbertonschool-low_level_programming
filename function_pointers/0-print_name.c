8433cca75640d6641ab21793ebd02940618c2697
xiomara13 <jamara0917@gmail.com>
2023-07-22 14:37:36 -0700

    name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - is the prototype
+ * @name: is the name
+ * @f: is the pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
