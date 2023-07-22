3117292389f55e4a39398dc9ccd1c452ed7f5504
xiomara13 <jamara0917@gmail.com>
2023-07-22 15:30:06 -0700

    elem

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,11 +5,16 @@
  * @array: array
  * @size: is the size
  * @action: is the argument
+ * Return;
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
