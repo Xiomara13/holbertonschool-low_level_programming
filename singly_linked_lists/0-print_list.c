02a500f28b24a2050f80fe255663e998dedcc82d
xiomara13 <jamara0917@gmail.com>
2023-08-02 18:10:41 -0700

    print

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t conteo = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		conteo++;
+	}
+	return (conteo);
+}
