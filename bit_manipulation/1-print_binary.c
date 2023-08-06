a4a523d3a405a72abb28555b1624640026823155
xiomara13 <jamara0917@gmail.com>
2023-08-05 20:50:56 -0700

    prints

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,10 +9,17 @@ void print_binary(unsigned long int n)
 	int i;
 	int num_bits = sizeof(unsigned long int) * 8;
 
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+
 	for (i = num_bits - 1; i >= 0; i--)
 	{
 		unsigned long int mask = 1UL << i;
 		unsigned long int bit = (n & mask) >> i;
+
 		putchar(bit + '0');
 	}
 }
