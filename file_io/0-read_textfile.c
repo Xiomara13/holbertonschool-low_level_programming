dabdb8e887e0afb63b84ad5e7adf0229e973ba83
xiomara13 <jamara0917@gmail.com>
2023-08-09 10:20:02 -0700

    file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include "main.h"
+/**
+ * read_textfile - Read and print text from a file
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ * Return: Number of letters read and printed, or 0 if an error occurs
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
